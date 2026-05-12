3ee4f78229c18b66cba71aa5a370276e6fa5fc4b
husseinmuhammadkh <hm4450969@gmail.com>
2026-05-12 21:23:28 +0300

    fix email

--- server/routes.ts
+++ server/routes.ts
@@ -9,19 +9,18 @@ import bcrypt from "bcryptjs";
 // 1. إعداد مرسل البريد
 // 1. إعداد مرسل البريد (نسخة مصححة لـ Render)
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
+  service: "gmail",
 
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    pass: process.env.EMAIL_PASS,
   },
-
-  family: 4,
-
-  tls: {
-    rejectUnauthorized: false
+});
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("EMAIL ERROR:", error);
+  } else {
+    console.log("EMAIL SERVER READY");
   }
 });
 
