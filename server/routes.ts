e762a129ed38f8adfa365e6b842c6935300b9102
husseinmuhammadkh <hm4450969@gmail.com>
2026-05-13 14:46:59 +0300

    fix gmail ipv4

--- server/routes.ts
+++ server/routes.ts
@@ -18,6 +18,10 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  tls: {
+    family: 4
+  }
 });
 
 transporter.verify((error, success) => {
